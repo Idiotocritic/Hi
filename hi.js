a3c7275b9f8989df19baa4bb3f5c2edff88affc6
Idiotocritic <atrayu.jmorgan@gmail.com>
2025-02-21 16:52:29 -0800

    Update hi.js

--- hi.js
+++ hi.js
@@ -1,1 +1,22 @@
+const textParagraph = document.getElementById('text');
 
+textParagraph.textContent = Math.random() * 1000;
+
+const btnElem = document.getElementById('btn1');
+    btnElem.addEventListener('click', function() {
+        textParagraph.textContent = Math.random() * 1000;
+    });
+    btnElem.addEventListener('load', function() {
+        textParagraph.style.color = 'red';
+    })
+
+    const colorPick = document.getElementById('colorPicker');
+
+
+
+    colorPick.addEventListener('mouseover' , function() {
+        const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+        colorPick.style.color = randomColor;
+        document.body.style.backgroundColor = colorPick.value;
+        document.getElementById('text').style.backgroundColor = colorPick.value;
+    })
